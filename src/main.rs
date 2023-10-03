08ca0173ec4b87023094d336e16b9ec7dd8ce1a4
DawnWT <dwnssb@gmail.com>
2023-10-03 15:35:52 +0200

    feat: get_headers fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{
+    env, fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+fn get_headers(mut stream: TcpStream) -> Vec<String> {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    return http_request;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(format!("127.0.0.1:8080")).unwrap();
+
+    println!("the server is running: 127.0.0.1:{server_port}");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+    }
 }
